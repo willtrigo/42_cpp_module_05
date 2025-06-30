384acbe6c992d652692623daacde1350df851611
dande-je <dande-je@student.42sp.org.br>
2025-06-30 16:08:16 -0300

    WIP: implement class form  Changes to be committed: 	modified:   src/core/form/Form.hpp

--- ex01/src/core/form/Form.hpp
+++ ex01/src/core/form/Form.hpp
@@ -6,7 +6,26 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 15:38:26 by dande-je          #+#    #+#             */
-/*   Updated: 2025/06/30 15:38:26 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/06/30 15:44:04 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef FORM_HPP
+#define FORM_HPP
+
+#include <exception>
+
+class Form {
+ public:
+  class GradeTooHighException : public std::exception {
+   public:
+    virtual const char* what() const throw();
+  };
+
+  class GradeTooLowException : public std::exception {
+   public:
+    virtual const char* what() const throw();
+  };
+};
+
+#endif // FORM_HPP
