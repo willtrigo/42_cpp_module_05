fa50691267a9fff2a87eb8a8b21aafff4130ee66
willtrigo <willtrigo@gmail.com>
2025-07-05 10:53:38 -0300

    Fix: improve text from exceptions  Changes to be committed: 	modified:   src/core/form/exceptions/FormGradeException.cpp

--- ex01/src/core/form/exceptions/FormGradeException.cpp
+++ ex01/src/core/form/exceptions/FormGradeException.cpp
@@ -6,18 +6,18 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 15:36:30 by dande-je          #+#    #+#             */
-/*   Updated: 2025/07/04 20:10:54 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/07/05 10:47:54 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core/form/Form.hpp"
 
 const char* Form::GradeTooHighException::what() const throw() {
-  return "Grade is too high for this form";
+  return "Grade is too high for this form (minimum grade is 1)";
 }
 
 const char* Form::GradeTooLowException::what() const throw() {
-  return "Grade is too low for this form";
+  return "Grade is too low for this form (maximum grade is 150)";
 }
 
 const char* Form::FormSignedException::what() const throw() {
