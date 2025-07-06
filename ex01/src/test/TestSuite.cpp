fb19eb80270a9a4be3bede99e6ce75288eb85b54
willtrigo <willtrigo@gmail.com>
2025-07-06 13:23:34 -0300

    Fix: change the title of the test  Changes to be committed: 	modified:   ../ex00/src/test/TestEx00.cpp 	modified:   ../ex00/src/test/TestSuite.cpp 	modified:   src/test/TestSuite.cpp

--- ex01/src/test/TestSuite.cpp
+++ ex01/src/test/TestSuite.cpp
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 15:30:45 by dande-je          #+#    #+#             */
-/*   Updated: 2025/07/04 20:25:40 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/07/06 13:20:04 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,9 @@
 #include "utils/TerminalColor.hpp"
 
 void runTests() {
-  testHelperLog(GREEN, "=== BUREAUCRAT TESTING SUITE ===");
+  testHelperLog(GREEN, std::string(TEST_LINE_SIZE, '='));
+  testHelperLog(GREEN, "BUREAUCRAT TESTING SUITE");
+  testHelperLog(GREEN, std::string(TEST_LINE_SIZE, '='));
   runAllEx00Tests();
   runAllEx01Tests();
   testHelperLog(GREEN, "\n" + std::string(TEST_LINE_SIZE, '='));
