97b68ed3b62617d8c2ef41e88da2ec90802cc2f1
willtrigo <willtrigo@gmail.com>
2025-07-06 13:13:45 -0300

    Test: implement test of form copy operations  Changes to be committed: 	modified:   src/test/TestEx01.cpp

--- ex01/src/test/TestEx01.cpp
+++ ex01/src/test/TestEx01.cpp
@@ -6,7 +6,7 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/30 15:29:27 by dande-je          #+#    #+#             */
-/*   Updated: 2025/07/06 12:19:49 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/07/06 13:12:44 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,6 +24,7 @@ static void testSuccessfulFormSigning();
 static void testInsufficientGradeToSigning();
 static void testAlreadySignedForm();
 static void testFormBoundaryConditions();
+static void testFormCopyOperations();
 
 void runAllEx01Tests() {
 	testBasicFormFunctionality();
@@ -32,6 +33,7 @@ void runAllEx01Tests() {
   testInsufficientGradeToSigning();
   testAlreadySignedForm();
   testFormBoundaryConditions();
+  testFormCopyOperations();
 }
 
 static void testBasicFormFunctionality() {
@@ -219,6 +221,44 @@ static void testFormBoundaryConditions() {
 
     std::cout << "After signing:" << std::endl;
     std::cout << "  " << normalForm << std::endl;
+  } catch (const std::exception& e) {
+    testHelperLog(ORANGE, "Caught unexpected exception:");
+    testHelperLog(RED, e.what());
+  }
+}
+
+static void testFormCopyOperations() {
+  testHelperPrintSection(PURPLE, "Form Copy Operations");
+
+  try {
+    Form original("Original Form", 42, 42);
+    Bureaucrat staff("Staff", 42);
+
+    std::cout << "Original Form: " << original << std::endl;
+
+    std::cout << std::endl;
+
+    original.beSigned(staff);
+    std::cout << "After signing original: " << original << std::endl;
+
+    std::cout << std::endl;
+
+    Form copy(original);
+    std::cout << "Copy constructed form: " << copy << std::endl;
+
+    std::cout << std::endl;
+
+    Form assigned("Temp Form", 1, 1);
+    std::cout << "Before assigment: " << assigned << std::endl;
+    assigned = original;
+    std::cout << "After assigment: " << assigned << std::endl;
+
+    std::cout << std::endl;
+
+    std::cout << "Verification of forms:" << std::endl;
+    std::cout << "  Original form: " << original << std::endl;
+    std::cout << "  Copy form: " << copy << std::endl;
+    std::cout << "  assigned form: " << assigned << std::endl;
   } catch (const std::exception& e) {
     testHelperLog(ORANGE, "Caught unexpected exception:");
     testHelperLog(RED, e.what());
