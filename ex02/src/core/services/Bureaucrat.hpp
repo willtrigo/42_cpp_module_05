6e8bad024d59f5e51f20c66bdad6b8452833dc34
dande-je <dande-je@student.42sp.org.br>
2025-07-06 20:48:11 -0300

    WIP: implement hpp of Bureaucrat  Changes to be committed: 	modified:   src/core/services/Bureaucrat.hpp

--- ex02/src/core/services/Bureaucrat.hpp
+++ ex02/src/core/services/Bureaucrat.hpp
@@ -6,11 +6,53 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/06 15:03:42 by dande-je          #+#    #+#             */
-/*   Updated: 2025/07/06 15:51:36 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/07/06 18:31:45 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef BUREAUCRAT_HPP
 #define BUREAUCRAT_HPP
+
+#include <exception>
+#include <string>
+
+class Bureaucrat {
+ public:
+  class GradeTooHighException : public std::exception {
+   public:
+    virtual const char* what() const throw();
+  };
+
+  class GradeTooLowException : public std::exception {
+   public:
+    virtual const char* what() const throw();
+  };
+
+  Bureaucrat();
+  Bureaucrat(const std::string& name, int grade);
+  Bureaucrat(const Bureaucrat& other);
+  Bureaucrat& operator=(const Bureaucrat& other);
+  ~Bureaucrat();
+
+  std::string getName() const;
+  int getGrade() const;
+
+  void incrementGrade();
+  void decrementGrade();
+
+ private:
+  const std::string m_name;
+  int m_grade;
+
+  static const std::string DEFAULT_NAME;
+  static const int DEFAULT_GRADE = 150;
+  static const int HIGHEST_GRADE = 1;
+  static const int LOWEST_GRADE = 150;
+  static const int GRADE_SHIFT_AMOUNT = 1;
+
+  void validateGrade(int grade) const;
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& Bureaucrat);
 
 #endif // BUREAUCRAT_HPP
