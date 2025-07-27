e0d626a3fa71723e18e3b92178c56f56cf0b484d
willtrigo <willtrigo@gmail.com>
2025-07-27 14:10:59 -0300

    Fix: jump line  Changes to be committed: 	modified:   src/application/commands/SignFormCommand.cpp

--- ex02/src/application/commands/SignFormCommand.cpp
+++ ex02/src/application/commands/SignFormCommand.cpp
@@ -6,13 +6,14 @@
 /*   By: dande-je <dande-je@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 18:03:28 by dande-je          #+#    #+#             */
-/*   Updated: 2025/07/19 23:54:36 by dande-je         ###   ########.fr       */
+/*   Updated: 2025/07/27 13:25:08 by dande-je         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "application/commands/SignFormCommand.hpp"
 #include "domain/models/bureaucrat/Bureaucrat.hpp"
 #include "domain/models/form/AForm.hpp"
+
 SignFormCommand::SignFormCommand(const AForm& form, const Bureaucrat& executor)
   : m_form(&form),
     m_executor(&executor) {}
